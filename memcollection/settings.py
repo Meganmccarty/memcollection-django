03be6083678599153d27090794174e86dba0ead6
Megan <megan_mccarty@hotmail.com>
2021-08-07 15:42:32 -0400

    Fix typo for rest_framework app

--- memcollection/settings.py
+++ memcollection/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'rest_framework',
     'django_filters',
     'taxonomy.apps.TaxonomyConfig'
 ]
