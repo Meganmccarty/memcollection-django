aa8872f56909a2b564c1295ae26872f9758dab67
Megan <megan_mccarty@hotmail.com>
2021-08-08 17:18:39 -0400

    Add tests for the subfamily model

--- taxonomy/tests/test_models.py
+++ taxonomy/tests/test_models.py
@@ -14,11 +14,20 @@ class TestTaxonomyModels(TestCase):
             common_name='Test Family Common Name',
             authority='Test Family Authority'
         )
+        subfamily = Subfamily.objects.create(
+            family=family,
+            name='Test Subfamily Name',
+            common_name='Test Subfamily Common Name',
+            authority='Test Subfamily Authority'
+        )
     def test_subclasses_order_model_base(self):
         self.assertTrue(issubclass(Order, TaxonomyBaseInfo))
     
     def test_subclasses_family_model_base(self):
         self.assertTrue(issubclass(Family, TaxonomyBaseInfo))
+    
+    def test_subclasses_subfamily_model_base(self):
+        self.assertTrue(issubclass(Subfamily, TaxonomyBaseInfo))
     
     def test_order_model_str(self):
         order = Order.objects.get(id=1)
@@ -27,3 +36,7 @@ class TestTaxonomyModels(TestCase):
     def test_family_model_str(self):
         family = Family.objects.get(id=1)
         self.assertEqual(str(family), 'Test Family Name')
+    
+    def test_subfamily_model_str(self):
+        subfamily = Subfamily.objects.get(id=1)
+        self.assertEqual(str(subfamily), 'Test Subfamily Name')
