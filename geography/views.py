4710eaa08b4c2869599593c235cde08013246988
Megan <megan_mccarty@hotmail.com>
2021-08-11 08:58:33 -0400

    Add more views

--- geography/views.py
+++ geography/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from geography.models import *
+from geography.serializers import *
 
-# Create your views here.
+class CountryList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+class StateList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+class CountyList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = County.objects.all()
+    serializer_class = CountySerializer
+
+class LocalityList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Locality.objects.all()
+    serializer_class = LocalitySerializer
