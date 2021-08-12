2d6c4d1b0d31c6c6f486c00d3ce0c196ea833656
Megan <megan_mccarty@hotmail.com>
2021-08-11 20:58:31 -0400

    Add person model to admin

--- specimens/admin.py
+++ specimens/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from specimens.models import *
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['full_name']
+    fields = ('first_name', 'middle_initial', 'last_name', 'suffix')
 
 # Register your models here.
