2433bf823a6094e7ebfa9eab01067b1641aac9cd
Megan <megan_mccarty@hotmail.com>
2021-08-12 15:13:22 -0400

    Add specimenrecord to admin

--- specimens/admin.py
+++ specimens/admin.py
@@ -6,4 +6,21 @@ class PersonAdmin(admin.ModelAdmin):
     list_display = ['full_name']
     fields = ('first_name', 'middle_initial', 'last_name', 'suffix')
 
-# Register your models here.
+@admin.register(SpecimenRecord)
+class SpecimenRecordAdmin(admin.ModelAdmin):
+    list_display = ('usi', 'species', 'locality', 'year', 'display_collectors', 'temp_C', 'temp_F')
+    fieldsets = (
+        ('Specimen Details', {
+            'fields': [
+                'usi', ('order', 'family', 'subfamily', 'tribe', 'genus', 'species', 'subspecies'),
+                ('determiner', 'determined_year'), ('sex', 'stage'), ('preparer', 'preparation', 'preparation_date'),
+                ('labels_printed', 'labeled', 'photographed', 'identified')
+            ]
+        }),
+        ('Locality Details', {
+            'fields': [
+                'collecting_trip', ('country', 'state', 'county', 'locality', 'gps'), ('day', 'month', 'year'),
+                'collector', 'method', ('weather', 'temperature', 'time_of_day'), 'habitat', 'notes'
+            ]
+        }),
+    )
