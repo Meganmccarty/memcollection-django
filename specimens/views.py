3f1daac624dc69dfe3251ec651f7669547a7287e
Megan <megan_mccarty@hotmail.com>
2021-08-12 16:47:40 -0400

    Create view for specimenrecords

--- specimens/views.py
+++ specimens/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from specimens.models import *
+from specimens.serializers import *
 
-# Create your views here.
+class SpecimenRecordList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = SpecimenRecord.objects.all()
+    serializer_class = SpecimenRecordSerializer
