4bbbac90dbd4caf265abc27b2e4994b1fdb84ed7
Megan <megan_mccarty@hotmail.com>
2021-08-13 14:15:34 -0400

    Add url for specimen record image

--- specimens/urls.py
+++ specimens/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from specimens import views
 
 urlpatterns = [
-    path('specimen-records/', views.SpecimenRecordList.as_view())
+    path('specimen-records/', views.SpecimenRecordList.as_view()),
+    path('specimen-record-images/', views.SpecimenRecordImageList.as_view())
 ]
