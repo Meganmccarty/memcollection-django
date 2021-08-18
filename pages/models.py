b8d601783c72beae22e08b73d4c85a03a7bcac92
Megan <megan_mccarty@hotmail.com>
2021-08-18 10:51:09 -0400

    Add models for page and reference

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from taxonomy.models import Species
 
-# Create your models here.
+class SpeciesPage(models.Model):
+    species = models.OneToOneField(Species, on_delete=models.SET_NULL, null=True,
+        related_name='species_page', help_text='Select the species for this page')
+    taxonomy = RichTextField(null=True, blank=True, help_text='Enter taxonomy details')
+    description = RichTextField(null=True, blank=True, help_text='Enter species description')
+    distribution = RichTextField(null=True, blank=True, help_text='Enter distribution details')
+    seasonality = RichTextField(null=True, blank=True, help_text='Enter seasonality details')
+    habitat = RichTextField(null=True, blank=True, help_text='Enter the species\' habitat')
+    food = RichTextField(null=True, blank=True, help_text='Enter info larval and adult food sources')
+    life_cycle = RichTextField(null=True, blank=True, help_text='Enter details about the life cycle')
+    references = models.ManyToManyField('Reference', related_name='species_pages',
+        help_text='Enter citations used to write the info in the preciding fields')
+
+    class Meta:
+        ordering = ['species']
+    
+    def __str__(self):
+        return f'{self.species} page'
+
+class Reference(models.Model):
+    title = models.CharField(max_length=50)
+    citation = RichTextField()
+
+    def __str__(self):
+        return f'{self.title}'
