f78fd7976a22ac8c881a2c62b07191dd32cf3ec6
Megan <megan_mccarty@hotmail.com>
2021-08-19 11:50:51 -0400

    Create views for image models

--- images/views.py
+++ images/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from images.models import InsectImage, PlantImage, HabitatImage
+from images.serializers import *
 
-# Create your views here.
+class InsectImageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = InsectImage.objects.all()
+    serializer_class = InsectImageSerializer
+
+class PlantImageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = PlantImage.objects.all()
+    serializer_class = PlantImageSerializer
+
+class HabitatImageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = HabitatImage.objects.all()
+    serializer_class = HabitatImageSerializer
