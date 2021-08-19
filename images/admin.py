777effb90510018b56c0c0e8537b7bf7eea7b393
Megan <megan_mccarty@hotmail.com>
2021-08-19 11:17:58 -0400

    Add image models to admin

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from images.models import InsectImage, PlantImage, HabitatImage
 
-# Register your models here.
+@admin.register(InsectImage)
+class InsectImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image')
+    fields = [
+        'image', 'name', 'caption', 'alt_text', 'datetime', 'other_notes', 'species', 'species_page',
+        'identified', 'sex', 'stage', 'status', 'country', 'state', 'county', 'locality', 'gps', 'collecting_trip'
+    ]
+
+@admin.register(PlantImage)
+class PlantImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image')
+    fields = [
+        'image', 'name', 'caption', 'alt_text', 'datetime', 'other_notes', 'species_page', 'latin_name',
+        'common_name', 'country', 'state', 'county', 'locality', 'gps', 'collecting_trip'
+    ]
+
+@admin.register(HabitatImage)
+class HabitatImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image')
+    fields = [
+        'image', 'name', 'caption', 'alt_text', 'datetime', 'other_notes', 'species_page',
+        'country', 'state', 'county', 'locality', 'gps', 'collecting_trip'
+    ]
+
