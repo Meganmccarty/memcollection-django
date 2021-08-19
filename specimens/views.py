ba003a0e9d618b56eeb6a23b0aab551df2d4bb25
Megan <megan_mccarty@hotmail.com>
2021-08-19 09:08:43 -0400

    Change to using viewsets, add filters

--- specimens/views.py
+++ specimens/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions
-from specimens.models import *
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from specimens.models import SpecimenRecord, SpecimenRecordImage
+from specimens.filters import *
 from specimens.serializers import *
 
-class SpecimenRecordList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class SpecimenRecordViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SpecimenRecord.objects.all()
     serializer_class = SpecimenRecordSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SpecimenRecordFilter
 
-class SpecimenRecordImageList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class SpecimenRecordImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SpecimenRecordImage.objects.all()
     serializer_class = SpecimenRecordImageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SpecimenRecordImageFilter
