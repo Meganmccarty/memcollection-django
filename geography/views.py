a9d14e5858d54cb45be60ee1bec6fd0f9fc9d26a
Megan <megan_mccarty@hotmail.com>
2021-08-19 10:12:00 -0400

    Change to using viewsets, add filters

--- geography/views.py
+++ geography/views.py
@@ -1,34 +1,41 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from geography.models import *
+from geography.filters import *
 from geography.serializers import *
 
-class CountryList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class CountryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CountryFilter
 
-class StateList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class StateViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = State.objects.all()
     serializer_class = StateSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = StateFilter
 
-class CountyList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class CountyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = County.objects.all()
     serializer_class = CountySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CountyFilter
 
-class LocalityList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class LocalityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Locality.objects.all()
     serializer_class = LocalitySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = LocalityFilter
 
-class GPSList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class GPSViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = GPS.objects.all()
     serializer_class = GPSSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = GPSFilter
 
-class CollectingTripList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class CollectingTripViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CollectingTrip.objects.all()
     serializer_class = CollectingTripSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CollectingTripFilter
