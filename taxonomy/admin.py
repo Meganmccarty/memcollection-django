275880f2325c888f105069342dcd280b81ad0089
Megan <megan_mccarty@hotmail.com>
2021-08-22 14:53:10 -0400

    Change fields displayed in list view

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -8,30 +8,30 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(Family)
 class FamilyAdmin(admin.ModelAdmin):
-    list_display = ('order', 'name', 'common_name', 'authority')
+    list_display = ('name', 'common_name', 'authority', 'order')
     fields = ('order', 'name', 'common_name', 'authority')
 
 @admin.register(Subfamily)
 class SubfamilyAdmin(admin.ModelAdmin):
-    list_display = ('family', 'name', 'common_name', 'authority')
+    list_display = ('name', 'common_name', 'authority', 'family')
     fields = ('family', 'name', 'common_name', 'authority')
 
 @admin.register(Tribe)
 class TribeAdmin(admin.ModelAdmin):
-    list_display = ('subfamily', 'name', 'common_name', 'authority')
+    list_display = ('name', 'common_name', 'authority', 'subfamily')
     fields = ('subfamily', 'name', 'common_name', 'authority')
 
 @admin.register(Genus)
 class GenusAdmin(admin.ModelAdmin):
-    list_display = ('tribe', 'name', 'common_name', 'authority')
+    list_display = ('name', 'common_name', 'authority', 'tribe')
     fields = ('tribe', 'name', 'common_name', 'authority')
 
 @admin.register(Species)
 class SpeciesAdmin(admin.ModelAdmin):
-    list_display = ('genus', 'name', 'common_name', 'authority', 'mona', 'p3')
+    list_display = ('get_binomial', 'common_name', 'authority', 'mona', 'p3')
     fields = ('genus', 'name', 'common_name', 'authority', 'mona', 'p3')
 
 @admin.register(Subspecies)
 class SubspeciesAdmin(admin.ModelAdmin):
-    list_display = ('species', 'name', 'common_name', 'authority', 'mona', 'p3')
+    list_display = ('get_trinomial', 'common_name', 'authority', 'mona', 'p3')
     fields = ('species', 'name', 'common_name', 'authority', 'mona', 'p3')
