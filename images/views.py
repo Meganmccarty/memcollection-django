f26ad6374a70b1668456beb9c384b81e62d0cfe3
Megan <megan_mccarty@hotmail.com>
2021-09-06 11:45:25 -0400

    Add filters to viewsets

--- images/views.py
+++ images/views.py
@@ -1,15 +1,23 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from images.models import InsectImage, PlantImage, HabitatImage
+from images.filters import *
 from images.serializers import *
 
 class InsectImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = InsectImage.objects.all()
     serializer_class = InsectImageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = InsectImageFilter
 
 class PlantImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PlantImage.objects.all()
     serializer_class = PlantImageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PlantImageFilter
 
 class HabitatImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = HabitatImage.objects.all()
     serializer_class = HabitatImageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = HabitatImageFilter
