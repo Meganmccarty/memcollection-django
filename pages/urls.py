aefa17d40d16dd6dcd9ede2362ab053f78086778
Megan <megan_mccarty@hotmail.com>
2021-11-01 18:38:24 -0400

    Add caching for species pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
+from django.conf.urls import include
+from django.views.decorators.cache import cache_page
 from rest_framework import routers
 from pages.views import *
 
 router = routers.DefaultRouter()
 router.register(r'species-pages', SpeciesPageViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('species-pages', cache_page(60 * 15)(SpeciesPageViewSet))
+]
