c10662a755c1a14e5cf39d9e603a0d52000bf9eb
Megan <megan_mccarty@hotmail.com>
2021-11-01 18:05:00 -0400

    Add caching for specimen records

--- specimens/urls.py
+++ specimens/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from django.conf.urls import include
+from django.views.decorators.cache import cache_page
 from rest_framework import routers
 from specimens.views import *
 
@@ -5,4 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'specimen-records', SpecimenRecordViewSet)
 router.register(r'specimen-record-images', SpecimenRecordImageViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('specimen-records', cache_page(60 * 15)(SpecimenRecordViewSet))
+]
