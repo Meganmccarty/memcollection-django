e151be6d91c8fb554a1a166f609ccad37d4aef07
Megan <megan_mccarty@hotmail.com>
2021-11-08 09:02:06 -0500

    Reduce SQL queries by using select_related and prefetch_related

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,9 @@ from pages.models import SpeciesPage
 from pages.serializers import SpeciesPageSerializer
 
 class SpeciesPageViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = SpeciesPage.objects.all()
+    queryset = SpeciesPage.objects.select_related(
+        'species', 'species__genus', 'species__genus__tribe', 'species__genus__tribe__subfamily',
+        'species__genus__tribe__subfamily__family', 'species__genus__tribe__subfamily__family__order'
+        ).prefetch_related('species__subspecies', 'insect_images', 'insect_images__species',
+        'habitat_images', 'plant_images', 'references')
     serializer_class = SpeciesPageSerializer
