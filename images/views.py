8407ceee10ea327df5577d8f2870c9b55fb2dd9e
Megan <megan_mccarty@hotmail.com>
2021-11-08 09:07:40 -0500

    Reduce SQL queries by using select_related and prefetch_related

--- images/views.py
+++ images/views.py
@@ -5,19 +5,19 @@ from images.filters import *
 from images.serializers import *
 
 class InsectImageViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = InsectImage.objects.all()
+    queryset = InsectImage.objects.select_related('species', 'country', 'state', 'county', 'locality', 'gps')
     serializer_class = InsectImageSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = InsectImageFilter
 
 class PlantImageViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = PlantImage.objects.all()
+    queryset = PlantImage.objects.select_related('country', 'state', 'county', 'locality', 'gps')
     serializer_class = PlantImageSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PlantImageFilter
 
 class HabitatImageViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = HabitatImage.objects.all()
+    queryset = HabitatImage.objects.select_related('country', 'state', 'county', 'locality', 'gps')
     serializer_class = HabitatImageSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = HabitatImageFilter
