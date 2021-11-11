86c06fd77fc0ed3c16682eca967281de5b76d1bf
Megan <megan_mccarty@hotmail.com>
2021-11-11 16:15:22 -0500

    Add species page filter to species page viewset

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from pages.models import SpeciesPage
+from pages.filters import SpeciesPageFilter
 from pages.serializers import SpeciesPageSerializer
 
 class SpeciesPageViewSet(viewsets.ReadOnlyModelViewSet):
@@ -9,3 +11,5 @@ class SpeciesPageViewSet(viewsets.ReadOnlyModelViewSet):
         ).prefetch_related('species__subspecies', 'insect_images', 'insect_images__species',
         'habitat_images', 'plant_images', 'references')
     serializer_class = SpeciesPageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = SpeciesPageFilter
