2826494cbcfd3a366a25cb03d39b47320f3d3768
Megan <megan_mccarty@hotmail.com>
2021-11-17 15:44:07 -0500

    Remove NestedSpeciesSerializer; instead use callable fields

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,6 +1,6 @@
 from serpy import Serializer, IntField, StrField, Field
 from images.serializers import ShortInsectImageSerializer, ShortPlantImageSerializer, ShortHabitatImageSerializer
-from taxonomy.serializers import NestedSpeciesSerializer
+from taxonomy.serializers import SpeciesSerializer
 
 class ReferencesSerializer(Serializer):
     id = IntField()
@@ -11,7 +11,12 @@ class SpeciesPageSerializer(Serializer):
     id = IntField()
     title = StrField()
     get_binomial = Field(call=True)
-    species = NestedSpeciesSerializer(required=True)
+    species = SpeciesSerializer(required=True)
+    genus = Field(call=True)
+    tribe = Field(call=True)
+    subfamily = Field(call=True)
+    family = Field(call=True)
+    order = Field(call=True)
     insect_images = ShortInsectImageSerializer(many=True, attr="insect_images.all", call=True, required=False)
     plant_images = ShortPlantImageSerializer(many=True, attr="plant_images.all", call=True, required=False)
     habitat_images = ShortHabitatImageSerializer(many=True, attr="habitat_images.all", call=True, required=False)
