d2627b2fca7c47a79ed579e4ec7ea822695302a0
Megan <megan_mccarty@hotmail.com>
2022-04-20 05:43:35 -0400

    Add filters and search fields

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from pages.models import SpeciesPage
 from pages.filters import SpeciesPageFilter
@@ -11,8 +11,9 @@ class SpeciesPageViewSet(viewsets.ReadOnlyModelViewSet):
         ).prefetch_related('species__subspecies', 'insect_images', 'insect_images__species',
         'habitat_images', 'plant_images', 'references')
     serializer_class = SpeciesPageSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_class = SpeciesPageFilter
+    search_fields = ['species__name', 'species__common_name', 'species__genus__name']
 
 class ShortenedSpeciesPageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SpeciesPage.objects.select_related(
