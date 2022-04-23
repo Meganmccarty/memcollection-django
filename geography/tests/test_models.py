a83e287c41ee17dc30cf9df204ed991e870f4424
Megan <megan_mccarty@hotmail.com>
2022-04-23 13:45:03 -0400

    Add tests for State using factories

--- geography/tests/test_models.py
+++ geography/tests/test_models.py
@@ -8,3 +8,14 @@ class TestCountryModel(TestCase):
     def test_country_str_equals_name(self):
         country_usa = factories.CountryFactory()
         self.assertEqual(country_usa.__str__(), 'United States of America')
+
+class TestStateModel(TestCase):
+    "Tests for State Model"
+
+    def test_state_str_equals_name(self):
+        state_indiana = factories.StateFactory()
+        self.assertEqual(state_indiana.__str__(), 'Indiana')
+    
+    def test_state_belongs_to_country(self):
+        state_indiana = factories.StateFactory()
+        self.assertEqual(state_indiana.country.name, 'United States of America')
