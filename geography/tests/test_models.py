72c9beb0e8bfde7be81f8d45af032c0e3420cb3b
Megan <megan_mccarty@hotmail.com>
2022-04-23 13:59:55 -0400

    Add tests for County using factories

--- geography/tests/test_models.py
+++ geography/tests/test_models.py
@@ -19,3 +19,27 @@ class TestStateModel(TestCase):
     def test_state_belongs_to_country(self):
         state_indiana = factories.StateFactory()
         self.assertEqual(state_indiana.country.name, 'United States of America')
+
+class TestCountyModel(TestCase):
+    "Tests for County Model"
+
+    def test_county_str_equals_name(self):
+        county_switzerland = factories.CountyFactory()
+        self.assertEqual(county_switzerland.__str__(), 'Switzerland')
+    
+    def test_county_belongs_to_state(self):
+        county_switzerland = factories.CountyFactory()
+        self.assertEqual(county_switzerland.state.name, 'Indiana')
+    
+    def test_render_county_abbr(self):
+        county = factories.CountyFactory()
+        alaska = factories.StateFactory(name='Alaska')
+        louisiana = factories.StateFactory(name='Louisiana')
+        borough = factories.CountyFactory(name='Fairbanks N. Star', state=alaska)
+        area = factories.CountyFactory(name='Yukon-Koyukuk Census Area', state=alaska)
+        parish = factories.CountyFactory(name='Allen', state=louisiana)
+
+        self.assertEqual(county.render_county_abbr(), 'Switzerland Co.')
+        self.assertEqual(borough.render_county_abbr(), 'Fairbanks N. Star Boro.')
+        self.assertEqual(area.render_county_abbr(), 'Yukon-Koyukuk Census Area')
+        self.assertEqual(parish.render_county_abbr(), 'Allen Par.')
