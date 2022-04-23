cb04a4aee7b1c517ccca6cafc4d2af379775ab28
Megan <megan_mccarty@hotmail.com>
2022-04-23 06:17:09 -0400

    Add tests for Order using factories

--- taxonomy/tests/test_models.py
+++ taxonomy/tests/test_models.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
-from taxonomy.models import *
+from taxonomy import factories
+from taxonomy.models import TaxonomyBaseInfo, Order, Family, Subfamily, Tribe, Genus, Species, Subspecies
+
+class TestOrderModel(TestCase):
+    "Tests for Order Model"
+
+    def test_order_subclasses_from_taxonomybase(self):
+        self.assertTrue(issubclass(Order, TaxonomyBaseInfo))
+
+    def test_order_str_equals_name(self):
+        order_lepidoptera = factories.OrderFactory()
+        self.assertEqual(order_lepidoptera.__str__(), 'Lepidoptera')
 
 class TestTaxonomyModels(TestCase):
     def setUp(self):
