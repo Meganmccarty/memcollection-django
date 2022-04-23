d34eafdc07b5e16cee66e5378d646dee4c95a903
Megan <megan_mccarty@hotmail.com>
2022-04-23 06:19:54 -0400

    Add tests for Family using factories

--- taxonomy/tests/test_models.py
+++ taxonomy/tests/test_models.py
@@ -11,3 +11,27 @@ class TestOrderModel(TestCase):
     def test_order_str_equals_name(self):
         order_lepidoptera = factories.OrderFactory()
         self.assertEqual(order_lepidoptera.__str__(), 'Lepidoptera')
+
+class TestFamilyModel(TestCase):
+    "Tests for Family Model"
+
+    def test_family_subclasses_from_taxonomybase(self):
+        self.assertTrue(issubclass(Family, TaxonomyBaseInfo))
+
+    def test_family_str_equals_name(self):
+        family_papilionidae = factories.FamilyFactory()
+        self.assertEqual(family_papilionidae.__str__(), 'Papilionidae')
+    
+    def test_family_belongs_to_order(self):
+        family_papilionidae = factories.FamilyFactory()
+        self.assertEqual(family_papilionidae.order.name, 'Lepidoptera')
+    
+    def test_family_image_url_with_no_image(self):
+        family_papilionidae = factories.FamilyFactory()
+        self.assertEqual(family_papilionidae.get_image_url(), '')
+    
+    def test_family_image_url_with_image(self):
+        family_papilionidae = factories.FamilyFactory(
+            image = 'butterfly.jpg',
+        )
+        self.assertIn(f'{family_papilionidae.image}', family_papilionidae.get_image_url())
