f92b69c10d10930c5fef0199b78c9838d410bc68
Megan <megan_mccarty@hotmail.com>
2022-08-14 17:26:20 -0400

    Modify image field on Family, refactor signals

--- taxonomy/signals.py
+++ taxonomy/signals.py
@@ -3,59 +3,38 @@ from django.dispatch import receiver
 from taxonomy.models import Order, Family, Subfamily, Tribe, Genus, Species, Subspecies
 from specimens.models import SpecimenRecord
 
-@receiver(post_save, sender=Order)
-def update_order_value(sender, instance, **kwargs):
+def update_specimen_record(instance, field):
     id = instance.id
-    specimens = SpecimenRecord.objects.filter(order=id)
-    
+    test = {field: id}
+    specimens = SpecimenRecord.objects.filter(**test)
+
     for specimen in specimens:
         specimen.save()
+
+@receiver(post_save, sender=Order)
+def update_order_value(sender, instance, **kwargs):
+    update_specimen_record(instance, "order")
 
 @receiver(post_save, sender=Family)
 def update_family_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(family=id)
-    
-    for specimen in specimens:
-        specimen.save()
+    update_specimen_record(instance, "family")
 
 @receiver(post_save, sender=Subfamily)
 def update_subfamily_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(subfamily=id)
-    
-    for specimen in specimens:
-        specimen.save()
+    update_specimen_record(instance, "subfamily")
 
 @receiver(post_save, sender=Tribe)
 def update_tribe_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(tribe=id)
-    
-    for specimen in specimens:
-        specimen.save()
+    update_specimen_record(instance, "tribe")
 
 @receiver(post_save, sender=Genus)
 def update_genus_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(genus=id)
-    
-    for specimen in specimens:
-        specimen.save()
+    update_specimen_record(instance, "genus")
 
 @receiver(post_save, sender=Species)
 def update_species_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(species=id)
-
-    for specimen in specimens:
-        specimen.save()
+    update_specimen_record(instance, "species")
 
 @receiver(post_save, sender=Subspecies)
 def update_subspecies_value(sender, instance, **kwargs):
-    id = instance.id
-    specimens = SpecimenRecord.objects.filter(subspecies=id)
-
-    for specimen in specimens:
-        specimen.save()
-
+    update_specimen_record(instance, "subspecies")
